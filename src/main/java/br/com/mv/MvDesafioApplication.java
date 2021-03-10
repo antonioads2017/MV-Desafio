d34e48da3889c1851de963b02d22decf88bb321d
Antonio Miguel <antoniofilhonerde@gmail.com>
2021-03-10 12:01:22 -0300

    iniciando projeto

---
+++ src/main/java/br/com/mv/MvDesafioApplication.java
@@ -0,0 +1,13 @@
+package br.com.mv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvDesafioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvDesafioApplication.class, args);
+    }
+
+}
